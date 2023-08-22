a3b10af6a693e9556011c0620f09371462f8c6b7
Meenatchi Selvaraj <2102767@sit.singaporetech.edu.sg>
2023-08-23 01:31:56 +0800

    Removed

--- faq_api/faq/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import QuestionTable, AnswerTable
-
-# Register your models here.
-admin.site.register(QuestionTable)
-admin.site.register(AnswerTable)
--- kiosk_api/library_catalogue/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import BookTable
-from .models import MemberTable
-from .models import BorrowTable
-
-# Register your models here.
-admin.site.register(BookTable)
-admin.site.register(MemberTable)
-admin.site.register(BorrowTable)
